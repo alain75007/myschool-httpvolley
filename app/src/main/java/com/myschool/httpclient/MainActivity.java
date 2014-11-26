041bbd0db16bb9f3caf5bb624f767752de75e075
Alain Beauvois <alain@itipic.com>
2014-11-26 12:27:21 +0100

    Configure project with Volley library and add JSON request

--- app/src/main/java/com/myschool/httpclient/MainActivity.java
+++ app/src/main/java/com/myschool/httpclient/MainActivity.java
@@ -1,12 +1,28 @@
 package com.myschool.httpclient;
 
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 
 public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,4 +53,69 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    public void httpGetClick(View view) {
+        // Tag used to cancel the request
+        String tag_json_obj = "json_obj_req";
+
+        String url = "http://api.androidhive.info/volley/person_object.json";
+
+        final ProgressDialog pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                        TextView textView = (TextView) findViewById(R.id.act_main_txtDisplay);
+                        textView.setText(response.toString());
+                        pDialog.hide();
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                // hide the progress dialog
+                pDialog.hide();
+            }
+        });
+
+        // Adding request to request queue
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjReq, tag_json_obj);
+
+    }
+    /*
+    public void login() {
+        final TextView mTxtDisplay;
+        ImageView mImageView;
+        mTxtDisplay = (TextView) findViewById(R.id.txtDisplay);
+        String url = "http://my-json-feed";
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+
+                (DownloadManager.Request.Method.GET, url, null, new Response.Listener() {
+
+                    @Override
+                    public void onResponse(Object response) {
+                        mTxtDisplay.setText("Response: " + ((JSONObject) response).toString());
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+
+// Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
+    }
+    */
 }
